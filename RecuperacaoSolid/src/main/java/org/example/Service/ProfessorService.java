1b822e454f67f2349092c7c64449dd85919646c5
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 15:38:51 -0300

    feat: Criado ProfessorService

--- RecuperacaoSolid/src/main/java/org/example/Service/ProfessorService.java
+++ RecuperacaoSolid/src/main/java/org/example/Service/ProfessorService.java
@@ -1,4 +1,34 @@
 package org.example.Service;
 
+import org.example.Model.Professor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProfessorService {
+    private List<Professor> professores = new ArrayList<>();
+
+    public void cadastrar(Professor professor) {
+        if (professor.getNome() == null || professor.getNome().isEmpty()) {
+            throw new RuntimeException("Nome do professor é obrigatório.");
+        }
+        professores.add(professor);
+    }
+
+    public List<Professor> listarTodos() {
+        return professores;
+    }
+
+    public Professor buscarPorId(int id) {
+        for (Professor p : professores) {
+            if (p.getId() == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public boolean eProfessorValido(int id) {
+        return buscarPorId(id) != null;
+    }
 }
