0e444208390bb09a2c661ca6b4bb81aa555e8f66
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 16:27:19 -0300

    feat: Criado o Usuario Repository

--- RecuperacaoSolid/src/main/java/org/example/Repository/UsuarioRepository.java
+++ RecuperacaoSolid/src/main/java/org/example/Repository/UsuarioRepository.java
@@ -1,4 +1,25 @@
 package org.example.Repository;
 
+import org.example.Model.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class UsuarioRepository {
+    // Lista que armazena qualquer tipo de usuário
+    private final List<Usuario> usuarios = new ArrayList<>();
+
+    public void salvar(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    public List<Usuario> buscarTodos() {
+        return new ArrayList<>(usuarios);
+    }
+
+    public Optional<Usuario> buscarPorId(int id) {
+        return usuarios.stream()
+                .filter(u -> u.getId() == id)
+                .findFirst();
+    }
 }
