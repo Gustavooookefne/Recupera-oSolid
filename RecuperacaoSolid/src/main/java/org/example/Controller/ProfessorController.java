f1a86571283b59f42d70fa7a570dd859ea60f429
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 16:28:54 -0300

    feat: Criado o professor controller

--- RecuperacaoSolid/src/main/java/org/example/Controller/ProfessorController.java
+++ RecuperacaoSolid/src/main/java/org/example/Controller/ProfessorController.java
@@ -1,4 +1,28 @@
 package org.example.Controller;
 
+import org.example.Dto.ProfessorDto.ProfessorRequestDto;
+import org.example.Dto.ProfessorDto.ProfessorResponseDto;
+import org.example.Service.ProfessorService;
+import org.example.mapper.ProfessorMapper;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ProfessorController {
+    private final ProfessorService professorService;
+    private final ProfessorMapper professorMapper;
+
+    public ProfessorController(ProfessorService service) {
+        this.professorService = service;
+        this.professorMapper = new ProfessorMapper();
+    }
+
+    public void cadastrar(ProfessorRequestDto request) {
+        professorService.cadastrar(professorMapper.toEntity(request));
+    }
+
+    public List<ProfessorResponseDto> listarTodos() {
+        return professorService.listarTodos().stream()
+                .map(professorMapper::toResponseDto)
+                .collect(Collectors.toList());
+    }
 }
