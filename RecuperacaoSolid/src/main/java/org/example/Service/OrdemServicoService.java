ae0c583c8069e92ce9f5f8e3ec2fb00852cb1035
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 14:49:52 -0300

    feat: Criado OrdemServicoService

--- RecuperacaoSolid/src/main/java/org/example/Service/OrdemServicoService.java
+++ RecuperacaoSolid/src/main/java/org/example/Service/OrdemServicoService.java
@@ -1,4 +1,38 @@
 package org.example.Service;
 
+import org.example.Model.OrdemServico;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrdemServicoService {
+    private List<OrdemServico> ordens = new ArrayList<>();
+
+    public void abrirOS(OrdemServico os) {
+        if (os.getProfessorResponsavel() != null) {
+            ordens.add(os);
+            System.out.println("OS " + os.getId() + " aberta com sucesso.");
+        } else {
+            throw new RuntimeException("Erro: Professor responsável é obrigatório.");
+        }
+    }
+
+    public boolean finalizarOS(int idOS, int idProfessor) {
+        for (OrdemServico os : ordens) {
+            if (os.getId() == idOS) {
+                if (os.getProfessorResponsavel().getId() == idProfessor) {
+                    os.setStatus("CONCLUIDA");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public List<OrdemServico> listarOrdens() {
+        return ordens;
+
+    }
+        public OrdemServico buscarPorId(int id) {
+        return ordens.stream().filter(o -> o.getId() == id).findFirst().orElse(null);
+    }
 }
