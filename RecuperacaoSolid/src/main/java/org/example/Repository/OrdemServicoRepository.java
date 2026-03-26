9b27afcd2934e2783c2831c2bd4646a999f60b1b
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 16:26:13 -0300

    feat: Criado o OrdemServico Repository

--- RecuperacaoSolid/src/main/java/org/example/Repository/OrdemServicoRepository.java
+++ RecuperacaoSolid/src/main/java/org/example/Repository/OrdemServicoRepository.java
@@ -1,4 +1,24 @@
 package org.example.Repository;
 
+import org.example.Model.OrdemServico;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrdemServicoRepository {
+    private final List<OrdemServico> ordens = new ArrayList<>();
+
+    public void salvar(OrdemServico os) {
+        ordens.add(os);
+    }
+
+    public List<OrdemServico> buscarTodas() {
+        return new ArrayList<>(ordens);
+    }
+
+    public OrdemServico buscarPorId(int id) {
+        return ordens.stream()
+                .filter(os -> os.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
 }
