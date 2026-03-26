2f782ee0a8cc16e27f7404d7961bbf92b50be861
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 14:35:23 -0300

    feat: Criado o Professor

---
+++ RecuperacaoSolid/src/main/java/org/example/Model/Professor.java
@@ -0,0 +1,27 @@
+package org.example.Model;
+
+public class Professor {
+    private int id;
+    private String nome;
+
+    public Professor(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
