5a632ba6968b3a9c50b00182434c92961c01e911
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 15:38:03 -0300

    feat: Criado AlunoService

--- RecuperacaoSolid/src/main/java/org/example/Service/AlunoService.java
+++ RecuperacaoSolid/src/main/java/org/example/Service/AlunoService.java
@@ -1,4 +1,30 @@
 package org.example.Service;
 
+import org.example.Model.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlunoService {
+    private List<Aluno> alunos = new ArrayList<>();
+
+    public void cadastrar(Aluno aluno) {
+        if (aluno.getNome() == null || aluno.getNome().isEmpty()) {
+            throw new RuntimeException("Nome do aluno é obrigatório.");
+        }
+        alunos.add(aluno);
+    }
+
+    public List<Aluno> listarTodos() {
+        return alunos;
+    }
+
+    public Aluno buscarPorId(int id) {
+        for (Aluno a : alunos) {
+            if (a.getId() == id) {
+                return a;
+            }
+        }
+        return null;
+    }
 }
