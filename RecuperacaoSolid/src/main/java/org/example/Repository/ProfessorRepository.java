165a0b6b09bd83d88c51cf2a47652f65c73c98b5
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 16:25:44 -0300

    feat: Criado o Professor Repository

--- RecuperacaoSolid/src/main/java/org/example/Repository/ProfessorRepository.java
+++ RecuperacaoSolid/src/main/java/org/example/Repository/ProfessorRepository.java
@@ -1,4 +1,22 @@
 package org.example.Repository;
 
+import org.example.Model.Professor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class ProfessorRepository {
+    private final List<Professor> professores = new ArrayList<>();
+
+    public void salvar(Professor professor) {
+        professores.add(professor);
+    }
+
+    public List<Professor> buscarTodos() {
+        return new ArrayList<>(professores);
+    }
+
+    public Optional<Professor> buscarPorId(int id) {
+        return professores.stream().filter(p -> p.getId() == id).findFirst();
+    }
 }
