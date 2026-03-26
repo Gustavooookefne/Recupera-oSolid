299e9b21c3b72c3fcc9ffd242c0f511a6864fe37
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 14:48:24 -0300

    feat: Criado TurmaService

--- RecuperacaoSolid/src/main/java/org/example/Service/TurmaService.java
+++ RecuperacaoSolid/src/main/java/org/example/Service/TurmaService.java
@@ -1,4 +1,27 @@
 package org.example.Service;
 
+import org.example.Model.Aluno;
+import org.example.Model.Turma;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TurmaService {
+    private List<Turma> turmas = new ArrayList<>();
+
+    public void criarTurma(Turma turma) {
+        turmas.add(turma);
+    }
+
+    public void adicionarAlunoNaTurma(int turmaId, Aluno aluno) {
+        for (Turma t : turmas) {
+            if (t.getId() == turmaId) {
+                t.getAlunos().add(aluno);
+                break;
+            }
+        }
+    }
+    public List<Turma> listarTodas() {
+        return turmas;
+    }
 }
