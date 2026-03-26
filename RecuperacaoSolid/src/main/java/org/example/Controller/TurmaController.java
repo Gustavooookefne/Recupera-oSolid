ed2b34f7221e9f159817a9f1216ef8794c3b83c9
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 16:30:20 -0300

    feat: Criado o Turma controller

--- RecuperacaoSolid/src/main/java/org/example/Controller/TurmaController.java
+++ RecuperacaoSolid/src/main/java/org/example/Controller/TurmaController.java
@@ -1,4 +1,28 @@
 package org.example.Controller;
 
+import org.example.Dto.TurmaDto.TurmaRequestDto;
+import org.example.Dto.TurmaDto.TurmaResponseDto;
+import org.example.Service.TurmaService;
+import org.example.mapper.TurmaMapper;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TurmaController {
+    private final TurmaService turmaService;
+    private final TurmaMapper turmaMapper;
+
+    public TurmaController(TurmaService service) {
+        this.turmaService = service;
+        this.turmaMapper = new TurmaMapper();
+    }
+
+    public void criarTurma(TurmaRequestDto request) {
+        turmaService.criarTurma(turmaMapper.toEntity(request));
+    }
+
+    public List<TurmaResponseDto> listarTodas() {
+        return turmaService.listarTodas().stream()
+                .map(turmaMapper::toResponseDto)
+                .collect(Collectors.toList());
+    }
 }
