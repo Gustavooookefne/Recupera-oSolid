d80d7cd9f7834bbc79dccf33c0e808e4214a7bf4
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-26 16:25:04 -0300

    feat: Criado o Aluno Repository

--- RecuperacaoSolid/src/main/java/org/example/Repository/AlunoRepository.java
+++ RecuperacaoSolid/src/main/java/org/example/Repository/AlunoRepository.java
@@ -1,4 +1,22 @@
 package org.example.Repository;
 
+import org.example.Model.Aluno;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class AlunoRepository {
+    private final List<Aluno> alunos = new ArrayList<>();
+
+    public void salvar(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    public List<Aluno> buscarTodos() {
+        return new ArrayList<>(alunos);
+    }
+
+    public Optional<Aluno> buscarPorId(int id) {
+        return alunos.stream().filter(a -> a.getId() == id).findFirst();
+    }
 }
